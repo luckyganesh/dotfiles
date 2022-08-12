f9a9b562ea12fa8f9b9885ce89842f39263fd54d
Sai Ganesh <saiganeb@thoughtworks.com>
2022-08-12 15:51:38 +0530

    Update the installation and the paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,7 +5,6 @@ source ~/.aliases
 fish_add_path /usr/local/opt/openjdk@11/bin
 export HOMEBREW_NO_AUTO_UPDATE=1
 
-export KUBECONFIG="/Users/saiganeb/.kube/config:/Users/saiganeb/.kube/configs/project.yaml"
 
  export GODEBUG=x509ignoreCN=0
 direnv hook fish | source
--- setup.fish
+++ setup.fish
@@ -1,10 +1,18 @@
+#!/usr/local/bin/fish
+
+rm ~/.tmux.conf ~/.vimrc ~/.gitconfig ~/.aliases ~/.lein
+rm -rf ~/.config ~/.lein
+
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-rm -rf ~/.config
 ln -s ~/dotfiles/config ~/.config
 ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/aliases ~/.aliases
+ln -s ~/dotfiles/lein ~/.lein
+
+
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 fisher install evanlucas/fish-kubectl-completions
