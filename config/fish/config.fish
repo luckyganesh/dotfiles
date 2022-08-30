fabe64030e410aca23db37debd41f05b3ab996cb
Sai Ganesh <saiganeb@thoughtworks.com>
2022-08-30 17:02:42 +0530

    pet,startship,tmux configs modified

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,9 +4,12 @@ source ~/.aliases
 
 fish_add_path /usr/local/opt/openjdk@11/bin
 export HOMEBREW_NO_AUTO_UPDATE=1
-
+function fish_user_key_bindings
+  bind \cs 'pet-select'
+end
 
  export GODEBUG=x509ignoreCN=0
 direnv hook fish | source
 fish_add_path /usr/local/opt/tomcat@9/bin
 starship init fish | source
+
